5f24400067d23c0d3d25152773aff26c01e5b7a0
AntoineTerre <44287949+AntoineTerre@users.noreply.github.com>
2020-04-14 15:28:58 +0200

    spring

--- src/HTMLparse.java
+++
@@ -1,6 +0,0 @@
-
-public class HTMLparse {
-	static class parse{
-		 
-	}
-}
---
+++ src/test/java/com/ggrecipes/searchrecipesservices/SearchrecipesservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggrecipes.searchrecipesservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchrecipesservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
